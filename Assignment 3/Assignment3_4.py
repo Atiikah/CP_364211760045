1b55ee57ad5f8a7337ee8de58c8fd7b137aeaf8d
atiikah <Atikah.s@rmutsvmail.com>
2022-03-22 09:20:10 +0700

    Test upload to github

--- Assignment 3/Assignment3_4.py
+++ Assignment 3/Assignment3_4.py
@@ -3,7 +3,6 @@ def getNumber():
     for x in range(5):
         listA.append(int(input(f'Enter integer [{x+1}]: ')))
     return listA
-
 mynum = getNumber()
 print(f'The data from user: {mynum}')
 print(f'The lowest number is: {min(mynum)}')
