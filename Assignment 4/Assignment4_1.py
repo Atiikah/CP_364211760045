cb5c8cd0794a85f845800432e618437a24380d99
atiikah <Atikah.s@rmutsvmail.com>
2022-04-07 22:51:06 +0700

    Test upload to github

--- Assignment 4/Assignment4_1.py
+++ Assignment 4/Assignment4_1.py
@@ -2,5 +2,5 @@ i = int(input("Enter number: "))
 count = 0
 while count < i:
     if i % 5 == 0:
-        print(f'The result is : ', count)
+        print(f'The result is  : ', count)
         count = count+5
