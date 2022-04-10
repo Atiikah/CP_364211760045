58055e9e549ed37ede62dcd923dbffc44edac4ae
atiikah <Atikah.s@rmutsvmail.com>
2022-04-10 13:51:31 +0700

    Test upload to github

---
+++ Final_Exam/final_exam_6.py
@@ -0,0 +1,25 @@
+"""
+Name: {atiikah saesoh}
+SID: {364211760045}
+Group: {MIT212}
+"""
+"""
+Quesiton 6:
+เขียนโปรแกรมเพื่อนับสระในภาษาอังกฤษ (Vowels: a e i o u) 
+โดยรับข้อความจากผู้ใช้ จากนั้นให้โปรแกรมแสดงผลว่าจากข้อความดังกล่าว
+มีสระแต่ละตัวมีจำนวนเท่าไร
+
+Example:
+
+Input message:  The National Vaccine Board approved a 
+policy with the additional target of procuring 150 million 
+doses of COVID-19 vaccines by 2022
+
+Result:
+a = 10
+e = 7
+I = 11
+o = 11
+u = 1 
+
+"""
